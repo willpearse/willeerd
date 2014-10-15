43a6f82bad2f83fa665d93035bae925fc2e8a63d
Will Pearse <will.pearse@gmail.com>
2014-10-15 09:13:44 -0500

    Fixed prog.bar - thanks Tiffany Knight!

--- R/utility.R
+++ R/utility.R
@@ -1,2 +1,2 @@
 prog.bar <- function(x, y)
-    if((x %% floor(y/100)) == 0) if((x %% (floor(y/100)*10)) == 0) cat("|") else cat(".")
+    tryCatch(if((x %% floor(y/100)) == 0) if((x %% (floor(y/100)*10)) == 0) cat("|") else cat("."), error=function(z) cat("."))
